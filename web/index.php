d2f844bcb14494667a0529ee22c5536473b5add3
Edu Poch <edupoch@gmail.com>
2016-10-24 15:16:49 +0200

    Más configuraciones para el entorno de desarrollo

--- web/index.php
+++ web/index.php
@@ -2,7 +2,10 @@
 
 if ($_SERVER['HTTP_HOST'] == 'localhost:8888' || $_SERVER['HTTP_HOST'] == 'localhost') {
 	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_ENV_DEV') or define('YII_ENV_DEV', true);
     defined('YII_ENV') or define('YII_ENV', 'local');
+} else {
+	define('YII_ENV_DEV', false);
 }
 
 require(__DIR__ . '/../vendor/autoload.php');
