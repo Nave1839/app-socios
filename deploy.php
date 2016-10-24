99d0748bf7a25bc20bbe121a94c4fd0556026654
Edu Poch <edupoch@gmail.com>
2016-10-24 14:41:27 +0200

    Añadimos la configuración del deployer

---
+++ deploy.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'recipe/yii2-app-basic.php';
+
+set('shared_files', ['config/db.php']);
+
+server('prod', '188.166.174.54', 22)
+    ->user('nave1839')
+    ->forwardAgent()
+    ->stage('production')
+    ->env('branch', 'master')
+    ->env('deploy_path', '/var/www/app.nave1839.org/www');
+
+set('repository', 'git@bitbucket.org:edupoch/app.nave1839.org.git');
